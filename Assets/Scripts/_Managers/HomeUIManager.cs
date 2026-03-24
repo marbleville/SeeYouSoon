ea091caf4bfaf4ce127250cad943c8ea1fe635db
carolinetpham <carolinetpham@outlook.com>
2026-03-24 11:02:17 -0400

    Add HomeUIManager script and OnClick functions

---
+++ Assets/Scripts/_Managers/HomeUIManager.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HomeUIManager : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] 
+    private GameObject mainMenuPanel;
+    [SerializeField] 
+    private GameObject instructionsPanel;
+    [SerializeField] 
+    private GameObject settingsPanel;
+
+    [Header("Instructions Dialogue")]
+    [SerializeField] 
+    private DialogueManager instructionsDialogueManager;
+
+    [Header("Scene Names")]
+    [SerializeField] 
+    private string firstLevelSceneName = "Level1_Office";
+
+    private void Start()
+    {
+        ShowMainMenu();
+        Time.timeScale = 1f;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevelSceneName);
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        instructionsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+
+        if (instructionsDialogueManager != null)
+        {
+            instructionsDialogueManager.EndDialogue();
+        }
+    }
+
+    public void ShowInstructions()
+    {
+        mainMenuPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+
+        if (instructionsDialogueManager != null)
+        {
+            instructionsDialogueManager.StartDialogue(
+                instructionsDialogueManager.introSpeakerName,
+                instructionsDialogueManager.introLines
+            );
+        }
+    }
+
+    public void ShowSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        instructionsPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+
+        if (instructionsDialogueManager != null)
+        {
+            instructionsDialogueManager.EndDialogue();
+        }
+    }
+}
