6311f49de7d9bd078ed5dc6800380411435b2389
Caroline Pham <carolinetpham@outlook.com>
2026-03-27 15:50:10 -0400

    Add bool to DM (#25)
    * Add bool to DM
    
    * Add bool to DM

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -16,10 +16,11 @@ public class DialogueManager : MonoBehaviour
     public string speakerName = "Guide";
 
     [TextArea(2, 5)]
-    public string[] dialogueLines;
+    public string[] dialogueLines; 
 
     [Header("Options")]
     public bool playIntroOnStart = false;
+    public bool loopDialogue = true;
 
     private string currentSpeaker;
     private string[] currentLines;
@@ -98,13 +99,23 @@ public class DialogueManager : MonoBehaviour
     {
         if (!isDialogueActive) return;
 
-        currentLineIndex++;
-
-        if (currentLineIndex >= currentLines.Length)
+        if (currentLineIndex >= currentLines.Length - 1)
         {
-            currentLineIndex = 0;
+            if (loopDialogue)
+            {
+                currentLineIndex = 0;
+                ShowCurrentLine();
+                UpdateArrows();
+            }
+            else
+            {
+                EndDialogue();
+            }
+
+            return;
         }
 
+        currentLineIndex++;
         ShowCurrentLine();
         UpdateArrows();
     }
