574cbae0439ef595215d748babaf36c8356af733
Christina Lin <lin.christ@northeastern.edu>
2026-03-28 05:34:07 -0400

    elevator scene change

--- Assets/Scripts/DocumentPickup.cs
+++ Assets/Scripts/DocumentPickup.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class DocumentPickup : APickupable
 {
+    new void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        base.Update();
+    }
+    
     public override void OnPickup()
     {
         GameEvents.TriggerDocumentsPickedUp();
