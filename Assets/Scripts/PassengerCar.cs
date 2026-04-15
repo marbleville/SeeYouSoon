f34e3f720700edfd117375a13b4ec8e240821bfc
marbleville <allen.ehrhardt@gmail.com>
2026-04-14 22:17:43 -0400

    added some draft cars on highway

---
+++ Assets/Scripts/PassengerCar.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PassengerCar : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public int startWaypointIdx = 0;
+
+    NavMeshAgent agent;
+    int currentWaypointIdx;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        currentWaypointIdx = startWaypointIdx;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Drive();
+    }
+
+    void Drive()
+    {
+        if (waypoints.Length == 0) return;
+
+        float waypointDist = Vector3.Distance(waypoints[currentWaypointIdx].position, transform.position);
+
+        if (waypointDist <= 30)
+        {
+            currentWaypointIdx++;
+            currentWaypointIdx = currentWaypointIdx % waypoints.Length;
+        }
+
+        agent.SetDestination(waypoints[currentWaypointIdx].position);
+    }
+}
